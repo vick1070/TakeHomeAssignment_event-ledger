db30a8d863fdb3646a1ed1944ec5178c142efcf4
shoptaki <vikas.sanhotra70@gmail.com>
2026-06-14 23:46:43 -0700

    bootstrap spring boot services

---
+++ event-gateway/src/test/java/com/eventledger/event_gateway/EventGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventledger.event_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
